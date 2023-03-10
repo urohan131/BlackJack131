d0c713635ee6e1eeeaacd3eb6f315f9345343afd
Rohan Kumar <urohan131@gmail.com>
2023-03-10 19:01:49 +0530

    Create Blackjack.py
    BlackJack

---
+++ Blackjack.py
@@ -0,0 +1,196 @@
+from random import shuffle
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 
+          'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,'Queen':10, 'King':10, 'Ace':11}
+
+playing = True
+
+class Card:
+    
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+    
+class Deck:
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+                
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__()  
+            
+        return 'The deck has:' + deck_comp
+    
+    def shuffle(self):
+        shuffle(self.deck)
+        
+    def deal(self):
+        return self.deck.pop()
+    
+    
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+        
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1
+            
+    def adjust_aces(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+            
+class Chips:
+    
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+        
+    def win_bet(self):
+        self.total += self.bet
+        
+    def loss_bet(self):
+        self.total -= self.bet
+        
+def take_bet(chips):
+    
+    while True:
+        try:
+            chips.bet = int(input("How many chips would you like to bet?"))   
+        except ValueError:
+            print("Sorry, a bet must be an intiger!") 
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed", chips.total)
+                
+            else:
+                break
+            
+def hit(deck,hand):
+    hand.add_card(deck.deal())
+    hand.adjust_aces()  
+    
+def hit_or_stand(deck, hand):
+    global playing
+    
+    while True:
+        x = input("Would you like to Hit or Stand? Enter h or s")
+        
+        if x[0].lower() == 'h':
+            hit(deck,hand)
+            
+        elif x[0].lower() == 's':
+            print("Player stands. Dealer is playing")
+            playing = False
+            
+        else:
+            print("Sorry, please try again.")
+            continue
+        break
+def show_some(player,dealer):
+    print("\n Dealer's Hand:")
+    print("<card hidden>")
+    print('',dealer.cards[1])
+    print("\nPlayer's Hand:", *player.cards, sep ='\n')
+    
+def show_all(player,dealer):
+    print("\nDealer's Hand:",*dealer.cards, sep ='\n')
+    print("\nDealer's Hand value= ",dealer.value)
+    print("\nPlayer's Hand:", *player.cards, sep = '\n')
+    print("\n Player's Hand Value=",player.value)
+    
+def player_busts(player,dealer,chips):
+    print("Player busts!")
+    chips.loss_bet()
+    
+def player_wins(player,dealer,chips):
+    print("Player Wins!")
+    chips.win_bet()
+    
+def dealer_busts(player,dealer,chips):
+    print("dealer busts!")
+    chips.win_bet()
+
+def dealer_wins(player,dealer,chips):
+    print("Dealer Wins!")
+    chips.loss_bet()
+    
+def push(dealer,player):
+    print("Dealer and Player tie! It's a push.")
+    
+while True:
+    print("Welcome to Blackjack131")
+    
+    deck = Deck()
+    deck.shuffle()
+    player_hand = Hand()
+    dealer_hand = Hand()
+    
+    player_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+    
+    player_chips = Chips()
+    
+    take_bet(player_chips)
+    
+    show_some(player_hand,dealer_hand)
+    while playing:
+        
+        hit_or_stand(deck,player_hand)
+        
+        show_some(player_hand,dealer_hand)
+        
+        if player_hand.value > 21:
+            player_busts(player_hand,dealer_hand,player_chips)
+            break
+        
+    if player_hand.value<= 21:
+        
+        while dealer_hand.value < 17:
+            
+            hit(deck,dealer_hand)
+            
+        show_all(player_hand, dealer_hand)
+        
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand,dealer_hand,player_chips)
+            
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand,dealer_hand,player_chips)
+            
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand,dealer_hand,player_chips)
+            
+        else:
+            push(player_hand,dealer_hand)
+    print("\n Player's winning stand at",player_chips.total)
+    
+    new_game = input("Would you like to play another game? Enter 'y' or 'n'")
+    
+    if new_game[0].lower()=='y':
+        playing = True
+        continue
+        
+    else:
+        print("Thanks for Playing")
+        break
+            
+    
+        
+        
